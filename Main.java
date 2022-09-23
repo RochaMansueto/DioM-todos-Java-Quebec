288ae24818b282a8fede90231fb6a22867d7c7fe
RochaMansueto <105565820+RochaMansueto@users.noreply.github.com>
2022-09-23 17:47:25 -0300

    Sobrecarga
    Second Commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+package one.digitalinnovation.basecamp.MAula2;
+
+public class Main {
+
+        public static void main(String[] args) {
+
+            // Quadrilátero
+            System.out.println("Exercício quadrilátero");
+            Quadrilatero.area(2);
+            Quadrilatero.area(5d,10d);
+            Quadrilatero.area(7,4,10);
+            Quadrilatero.area(5f,5f);
+
+        }
+    }
