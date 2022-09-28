c070e6ce383c66ef3ea01a9bc460caa14bf1b6bc
RochaMansueto <105565820+RochaMansueto@users.noreply.github.com>
2022-09-27 20:45:20 -0400

    Tratamento Excecoes Java

---
+++ TratamentoExcecoesJava/DivisaoNaoExataException.java
@@ -0,0 +1,13 @@
+package JavaQuebec.TratamentoExcecoesJava;
+
+public class DivisaoNaoExataException {
+    private int numerado;
+    private int denominador;
+
+    public DivisaoNaoExataException(String message, int numerado, int denominador) {
+        super(message);
+        this.numerado = numerado;
+        this.denominador = denominador;
+    }
+}
+
